7deda95af4fbc09173fd191932f42b1e82ed209e
hzchenzhe1 <hzchenzhe1@netease.com>
2017-12-28 09:33:08 +0800

    Strings

---
+++ src/main/java/utils/Strings.java
@@ -0,0 +1,36 @@
+public class Strings {
+
+// 两个方法是AbstractStringBuffer 中 的reverse , StringBuffer的parent, 实现方式值的参考
+  public StringBuffer reverse(int count, char[] value) {
+	boolean hasSurrogates = false;
+        int n = count - 1;
+        for (int j = (n-1) >> 1; j >= 0; j--) {
+            int k = n - j;
+            char cj = value[j];
+            char ck = value[k];
+            value[j] = ck;
+            value[k] = cj;
+            if (Character.isSurrogate(cj) ||
+                Character.isSurrogate(ck)) {
+                hasSurrogates = true;
+            }
+        }
+        if (hasSurrogates) {
+            reverseAllValidSurrogatePairs();
+        }
+        return new StringBuffer(value);
+  }
+
+   private void reverseAllValidSurrogatePairs(int count, char[] value) {
+        for (int i = 0; i < count - 1; i++) {
+            char c2 = value[i];
+            if (Character.isLowSurrogate(c2)) {
+                char c1 = value[i + 1];
+                if (Character.isHighSurrogate(c1)) {
+                    value[i++] = c1;
+                    value[i] = c2;
+                }
+            }
+        }
+    }
+}
